e298a7a4ba6b5964d05567a721f47bcee7fec40d
Andre Barreto <andre.barreto@cnj.jus.br>
2023-03-16 14:48:28 -0300

    Teste Rebase

--- src/main/java/br/com/estudo/enums/Main.java
+++ src/main/java/br/com/estudo/enums/Main.java
@@ -3,6 +3,7 @@ package br.com.estudo.enums;
 public class Main {
     public static void main(String[] args) {
         System.out.println(HelloEnum.INDEFINIDO.name());
+        System.out.println(HelloEnum.valueOf(HelloEnum.INDEFINIDO.name()));
     }
     private void teste(){}
 
